63592140757831ef4c4dd7b6f98e08e9a54453d3
denis <denis.huber@dudagroup.com>
2017-09-04 11:57:19 +0200

    added new migration functions tests

--- continuousMigrations_spec.js
+++ continuousMigrations_spec.js
@@ -95,11 +95,6 @@ function createTestModel (signature) {
                     "testModel": {
                       "contextual": {
                         "struct": {
-                          "myOptional": {
-                            "optional": {
-                              "string": {}
-                            }
-                          },
                           "myString": {
                             "string": {}
                           },
@@ -108,23 +103,21 @@ function createTestModel (signature) {
                           },
                           "myFloat": {
                             "float": {}
+                          }
+                        }
+                      }
+                    },
+                    "testModelMigrated": {
+                      "contextual": {
+                        "struct": {
+                          "myString": {
+                            "string": {}
                           },
-                          "myUnion": {
-                            "union": {
-                              "caseA": {
-                                "string": {}
-                              }
-                            }
+                          "myInt": {
+                            "int": {}
                           },
-                          "myStruct": {
-                            "struct": {
-                              "myKeyA": {
-                                "string": {}
-                              }
-                            }
-                          },
-                          "myRef": {
-                            "ref": "refTestModel"
+                          "myFloat": {
+                            "float": {}
                           }
                         }
                       }
@@ -137,7 +130,7 @@ function createTestModel (signature) {
                           }
                         }
                       }
-                    }
+                    },
                   }
                 }
               },
@@ -208,7 +201,6 @@ function createEntries (signature) {
               "values": {
                 "a": {
                   "newStruct": {
-                    "myOptional": "optional string",
                     "myString": {
                       "newString": "foo"
                     },
@@ -217,71 +209,6 @@ function createEntries (signature) {
                     },
                     "myFloat": {
                       "newFloat": 5.55
-                    },
-                    "myUnion": {
-                      "newUnion": {
-                        "caseA": "myCaseAString"
-                      }
-                    },
-                    "myStruct": {
-                      "newStruct": {
-                        "myKeyA": {
-                          "newString": "myStructKeyA"
-                        }
-                      }
-                    },
-                    "myRef": {
-                      "create": {
-                        "in": {
-                          "tag": "refTestModel"
-                        },
-                        "value": {
-                          "newStruct": {
-                            "myString": {
-                              "newString": "test reference"
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                },
-                "b": {
-                  "newStruct": {
-                    "myString": {
-                      "newString": "bar"
-                    },
-                    "myInt": {
-                      "newInt": 555
-                    },
-                    "myFloat": {
-                      "newFloat": 5.55
-                    },
-                    "myUnion": {
-                      "newUnion": {
-                        "caseA": "myCaseAString"
-                      }
-                    },
-                    "myStruct": {
-                      "newStruct": {
-                        "myKeyA": {
-                          "newString": "myStructKeyA"
-                        }
-                      }
-                    },
-                    "myRef": {
-                      "create": {
-                        "in": {
-                          "tag": "refTestModel"
-                        },
-                        "value": {
-                          "newStruct": {
-                            "myString": {
-                              "newString": "test reference"
-                            }
-                          }
-                        }
-                      }
                     }
                   }
                 }
@@ -303,205 +230,46 @@ function createEntries (signature) {
 
 
 function makeMigration (signature) {
-  frisby.create('function unsafeMutateModel')
+  frisby.create('function create _migration')
     .post(KARMA_ENDPOINT, null,
       {
         json: false,
         body: JSON.stringify(
           {
-            "unsafeMutateModel": {
-              "model": {
-                "tag": "testModel"
+            "create": {
+              "in": {
+                "tag": "_migration"
               },
-              "expression": {
+              "value": {
                 "newStruct": {
-                  "myString": {
-                    "field": "myString"
-                  },
-                  "myRenamedString": {
-                    "field": "myString"
-                  },
-                  "myInt": {
-                    "field": "myInt"
-                  },
-                  "myNewInt": {
-                    "floatToInt": {
-                      "field": "myFloat"
-                    }
-                  },
-                  "myFloat": {
-                    "field": "myFloat"
-                  },
-                  "myNewFloat": {
-                    "add": [
-                      {
-                        "intToFloat": {
-                          "field": "myInt"
-                        }
-                      },
-                      {
-                        "field": "myFloat"
-                      }
-                    ]
-                  },
-                  "myStruct": {
-                    "newStruct": {
-                      "myKeyA": {
-                        "field": {
-                          "name": "myKeyA",
-                          "value": {
-                            "field": "myStruct"
-                          }
-                        }
-                      },
-                      "myKeyB": {
-                        "newInt": 888
-                      }
-                    }
-                  },
-                  "myUnion": {
-                    "newUnion": {
-                      "caseA": {
-                        "assertCase": {
-                          "case": "caseA",
-                          "value": {
-                            "field": "myUnion"
-                          }
-                        }
-                      }
-                    }
-                  },
-                  "myRef": {
-                    "field": "myRef"
-                  },
-                  "myNewBool": {
-                    "newBool": true
-                  },
-                  "myNewString": {
-                    "newString": "Lorem ipsum"
-                  }
-                }
-              }
-            }
-          }
-        )
-      }
-    )
-    .addHeader('X-Karma-Signature', signature)
-    .addHeader('X-Karma-Database', dbName)
-    .addHeader('X-Karma-Codec', 'json')
-    .expectStatus(200)
-    .afterJSON(function (json) {
-      getTestModelRecords(signature)
-    })
-    .inspectBody()
-    .toss();
-}
-
-
-function getTestModelRecords (signature) {
-  frisby.create('get model entires')
-    .post(KARMA_ENDPOINT, null,
-      {
-        json: false,
-        body: JSON.stringify(
-          {
-            "first": {
-              "filter": {
-                "value": {
-                  "all": {
+                  "from": {
                     "tag": "testModel"
-                  }
-                },
-                "expression": {
-                  "equal": [
-                    "foo",
-                    {
-                      "field": "myString"
-                    }
-                  ]
-                }
-              }
-            }
-          }
-        )
-      }
-    )
-    .addHeader('X-Karma-Signature', signature)
-    .addHeader('X-Karma-Database', dbName)
-    .addHeader('X-Karma-Codec', 'json')
-    .expectStatus(200)
-    .expectJSON(
-      {
-        "myFloat": 5.55,
-        "myInt": 555,
-        "myNewBool": true,
-        "myNewFloat": 560.55,
-        "myNewInt": 5,
-        "myNewString": "Lorem ipsum",
-        "myRenamedString": "foo",
-        "myString": "foo",
-        "myStruct": {"myKeyA": "myStructKeyA", "myKeyB": 888},
-        "myUnion": {"caseA": "myCaseAString"}
-      }
-    )
-    .expectJSONTypes({
-      myRef: String
-    })
-    .inspectBody()
-    .afterJSON(function (json) {
-      getTestModel(signature)
-    })
-    .toss();
-}
-
-function getRefModelRecords (signature) {
-  frisby.create('get model entires')
-    .post(KARMA_ENDPOINT, null,
-      {
-        json: false,
-        body: JSON.stringify(
-          {
-            "all": {
-              "tag": "refTestModel"
-            }
-          }
-        )
-      }
-    )
-    .addHeader('X-Karma-Signature', signature)
-    .addHeader('X-Karma-Database', dbName)
-    .addHeader('X-Karma-Codec', 'json')
-    .expectStatus(200)
-    .inspectBody()
-    .toss();
-}
-
-function getTestModel (signature) {
-  frisby.create('get model entires')
-    .post(KARMA_ENDPOINT, null,
-      {
-        json: false,
-        body: JSON.stringify(
-          {
-            "first": {
-              "filter": {
-                "value": {
-                  "all": {
-                    "tag": "_model"
-                  }
-                },
-                "expression": {
-                  "equal": [
-                    {
-                      "refTo": {
-                        "id": {}
+                  },
+                  "to": {
+                    "tag": "testModelMigrated"
+                  },
+                  "expression": {
+                    "create": {
+                      "in": {
+                        "tag": "_expression"
+                      },
+                      "value": {
+                        "static": {
+                          "newStruct": {
+                            "myString": {
+                              "field": "myString"
+                            },
+                            "myInt": {
+                              "field": "myInt"
+                            },
+                            "myFloat": {
+                              "field": "myFloat"
+                            }
+                          }
+                        }
                       }
-                    },
-                    {
-                      "tag": "testModel"
                     }
-                  ]
+                  }
                 }
               }
             }
@@ -513,53 +281,10 @@ function getTestModel (signature) {
     .addHeader('X-Karma-Database', dbName)
     .addHeader('X-Karma-Codec', 'json')
     .expectStatus(200)
-    .expectJSON(
-      {
-        "struct": {
-          "myFloat": {
-            "float": {}
-          },
-          "myInt": {
-            "int": {}
-          },
-          "myNewBool": {
-            "bool": {}
-          },
-          "myNewFloat": {
-            "float": {}
-          },
-          "myNewInt": {
-            "int": {}
-          },
-          "myNewString": {
-            "string": {}
-          },
-          "myRenamedString": {
-            "string": {}
-          },
-          "myString": {
-            "string": {}
-          },
-          "myStruct": {
-            "struct": {
-              "myKeyA": {
-                "string": {}
-              },
-              "myKeyB": {
-                "int": {}
-              }
-            }
-          },
-          "myUnion": {
-            "union": {
-              "caseA": {
-                "string": {}
-              }
-            }
-          }
-        }
-      }
-    )
+    .afterJSON(function (json) {
+      //getTestModelRecord(signature)
+    })
     .inspectBody()
     .toss();
 }
+
