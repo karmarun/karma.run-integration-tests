fe8f565166101f09c4e76a09c7174c53c1ebfdfe
denis <denis.huber@dudagroup.com>
2018-07-24 17:15:11 +0200

    extract queries script

--- scripts/extractDocuExampleQueries.js
+++ scripts/extractDocuExampleQueries.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
-const {Client} = require('@karma.run/sdk')
 const {execCommand, spanProcess} = require('./tools.js')
-const {KARMA_ENDPOINT, KARMA_INSTANCE_SECRET} = process.env;
 
 main()
   .catch(function (error) {
@@ -9,8 +7,6 @@ main()
   })
 
 async function main() {
-  const client = new Client(KARMA_ENDPOINT)
-  await client.authenticate('admin', KARMA_INSTANCE_SECRET)
   await execCommand("rm -rf ./temp/queries")
   await execCommand("mkdir ./temp/queries")
   await spanProcess("EXTRACT_QUERIES=true yarn", ["test"])
